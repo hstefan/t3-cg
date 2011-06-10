f66e3683f724c0880ac587d39a3f43a04fb393f8
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-09 22:39:17 -0300

    Configuring project in order to work correctly with SCV.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <SCV/Button.h>
+#include <SCV/Kernel.h>
 
 int main() 
 {
-   std::cout << "TESTE" << std::endl;
+   scv::Kernel* ker = scv::Kernel::getInstance();
+   scv::ColorScheme* scheme = scv::ColorScheme::getInstance();
+   scheme->loadScheme(scv::ColorScheme::clean);
+   ker->setWindowSize(800, 600);
+
+   scv::Button* b = new scv::Button(scv::Point(400, 300), "Clique!");
+   ker->addComponent(b);
+   ker->setFramesPerSecond(30.f);
+   ker->run();
 }
