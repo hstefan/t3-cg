46f13e75a4a8c60041fe914d5f5788aae4dad1d0
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-13 21:51:39 -0300

    math/vector.hpp
    unhomogenize and homogenize methods

---
+++ src/util/TransformStack.cpp
@@ -0,0 +1,77 @@
+/**********************************************************************************
+ * Copyright (C) 2011 by Hugo Stefan Kaus Puhlmann                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          * 
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in     *
+ * all copies or substantial portions of the Software.                            *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN      *
+ * THE SOFTWARE.                                                                  *
+ *********************************************************************************/
+
+/*
+ * Nome: Hugo Stefan Kaus Puhlmann
+ * Matricula: 2910182
+ */
+
+#include "TransformStack.hpp"
+#include "../math/transform3d.hpp"
+
+using namespace hstefan::util;
+
+void TransformStack::pushYaw(float angle)
+{
+   pushCustom(math::yawRotationMatrix(angle));
+}
+
+void TransformStack::pushRoll(float angle)
+{
+   pushCustom(math::rollRotationMatrix(angle));
+}
+
+void TransformStack::pushPitch(float angle)
+{
+   pushCustom(math::pitchRotationMatrix(angle));
+}
+
+void TransformStack::pushScale(float sx, float sy, float sz)
+{
+   pushCustom(math::scaleMatrix(sx, sy, sz));
+}
+
+void TransformStack::pushTranslate(float tx, float ty, float tz)
+{
+   pushCustom(math::translationMatrix(tx, ty, tz));
+}
+
+void TransformStack::pushCustom(const transf_type& custom_transf)
+{
+   transformations.push_back(custom_transf);
+   changed = true;
+}
+
+const std::vector<TransformStack::vertex_type>* const TransformStack::apply()
+{
+   math::mat4d res = math::identifyMatrix();
+   auto end_it = transformations.rend();
+   for(auto it = transformations.rbegin(); it != end_it; ++it)
+      res = res * (*it);
+   auto end_it_v = vertex_buff.end();
+   out_buff->clear();
+   vertex_type aux;
+   for(auto it = vertex_buff.begin(); it != end_it_v; ++it) 
+   {
+      out_buff->push_back((*it) * res);
+   }
+   return 0;
+}
