03d30a49b7147f859904e295ff5a3c601978ed9a
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-13 21:57:41 -0300

    Commenting my failed attempt to implement a matrix cast

--- src/math/vector.hpp
+++ src/math/vector.hpp
@@ -145,6 +145,7 @@ inline matrix<M - 1, 1> unhomogen(const matrix<M, 1>& v)
    return res;
 }
 
+/*
 template <unsigned int M>
 matrix<M - 1, 1> operator matrix<M, 1>(const matrix<M, 1>& v)
 {
@@ -152,7 +153,7 @@ matrix<M - 1, 1> operator matrix<M, 1>(const matrix<M, 1>& v)
    for(unsigned int i = 0; i < M - 1; ++i)
       res[i] = v[i];
    return res;
-}
+}*/
 
 
 } //namespace math
